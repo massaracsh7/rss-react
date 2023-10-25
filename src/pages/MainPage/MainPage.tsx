7334231cf52d5221a0783ee19dd2de1a8c194910
massaracsh7 <fatum7@gmail.com>
2023-10-25 11:21:59 +0300

    feat: add main page

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <div className='main-page'>
+        <h1>Header1</h1>
+      </div>
+    );
+  }
+}
