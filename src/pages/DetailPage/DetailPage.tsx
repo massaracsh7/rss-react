f3de713583384c09427cc9785ae0a1397a923024
massaracsh7 <fatum7@gmail.com>
2023-11-04 14:36:57 +0300

    feat: add close detailPage

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { DetailInfo } from '../../components/DetailInfo';
 import { Loader } from '../../components/Loader';
 import { Character, defaultCharacter } from '../../types/types';
 import { getCharacter } from '../../utils/api';
-import DetailInfo from './DetailInfo';
 import './style.css';
 
 export default function DetailPage() {
@@ -12,6 +12,7 @@ export default function DetailPage() {
   const [loading, setLoading] = useState(false);
   const params = useParams();
   const detailId = params.id ?? 0;
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getData = async () => {
@@ -35,5 +36,23 @@ export default function DetailPage() {
     return <DetailInfo character={character} />;
   }, [character, loading]);
 
-  return <div className='cards__detail'>{viewDetails}</div>;
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        navigate('/', { replace: true });
+      }
+    };
+    document.addEventListener('mousedown', handleClick);
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [navigate]);
+
+  return (
+    <div className='cards__detail' ref={ref}>
+      {viewDetails}
+    </div>
+  );
 }
