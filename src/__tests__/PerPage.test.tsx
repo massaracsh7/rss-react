4d807c4ebf5da4aaa0cb632ff49485c88787872c
massaracsh7 <fatum7@gmail.com>
2023-11-09 21:35:52 +0300

    feat: add Loader, perpage tests

---
+++ src/__tests__/PerPage.test.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { PerPage } from '../components/PerPage';
+
+describe('PerPage', () => {
+  test('should render options count items', () => {
+    render(<PerPage setCountItems={jest.fn()} />);
+    const pageItems = screen.getByTestId('per-page');
+    expect(pageItems).toBeInTheDocument();
+  });
+
+  test('should change options count 20', () => {
+    render(<PerPage setCountItems={jest.fn()} />);
+    const choose = screen.getByText('20');
+    userEvent.click(choose);
+    waitFor(() => {
+      expect(screen.getByRole('form')).toHaveFormValues({ value: '20' });
+    });
+  });
+  test('should change options count 5', () => {
+    render(<PerPage setCountItems={jest.fn()} />);
+    const choose = screen.getByText('5');
+    userEvent.click(choose);
+    waitFor(() => {
+      expect(screen.getByRole('form')).toHaveFormValues({ value: '5' });
+    });
+  });
+});
--- src/components/PerPage/PerPage.tsx
+++ src/components/PerPage/PerPage.tsx
@@ -6,7 +6,11 @@ interface Props {
 
 export default function PerPage({ setCountItems }: Props) {
   return (
-    <select className='count' onChange={(event) => setCountItems(event.target.value)}>
+    <select
+      className='count'
+      onChange={(event) => setCountItems(event.target.value)}
+      data-testid='per-page'
+    >
       <option value='20'>20</option>
       <option value='15'>15</option>
       <option value='10'>10</option>
