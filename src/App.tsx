44969697be2ae2a2bec4e20c05deb6f4dd34dda7
massaracsh7 <fatum7@gmail.com>
2023-11-13 15:00:06 +0300

    feat: add store in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,22 @@
-import { useState } from 'react';
+import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
-import { ItemsContext, ItemsDefault } from './contexts/ItemsContext';
-import { SearchContext, SearchDefault } from './contexts/SearchContext';
 import DetailPage from './pages/DetailPage/DetailPage';
 import MainPage from './pages/MainPage/MainPage';
 import Page404 from './pages/Page404/Page404';
+import store from './store';
 
 export default function App() {
-  const [searchData, setSearchData] = useState(SearchDefault);
-  const [itemsData, setItemsData] = useState(ItemsDefault);
-
   return (
     <>
-      <SearchContext.Provider
-        value={{
-          searchData,
-          setSearchData,
-        }}
-      >
-        <ItemsContext.Provider
-          value={{
-            itemsData,
-            setItemsData,
-          }}
-        >
-          <Routes>
-            <Route path='/' element={<MainPage />}>
-              <Route path='details=/:id' element={<DetailPage />} />
-            </Route>
-            <Route path='*' element={<Page404 />} />
-          </Routes>
-        </ItemsContext.Provider>
-      </SearchContext.Provider>
+      <Provider store={store}>
+        <Routes>
+          <Route path='/' element={<MainPage />}>
+            <Route path='details=/:id' element={<DetailPage />} />
+          </Route>
+          <Route path='*' element={<Page404 />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
