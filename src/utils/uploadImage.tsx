b1b2176b462319625d808a5abeb4354553f67b16
massaracsh7 <fatum7@gmail.com>
2023-12-03 14:59:29 +0300

    fix: fix errors

--- src/utils/uploadImage.tsx
+++ src/utils/uploadImage.tsx
@@ -1,12 +1,12 @@
 import { file64 } from './file64';
 
-const allowedMimes = ['image/png', 'image/jpg', 'image/jpeg'];
+const types = ['image/png', 'image/jpg', 'image/jpeg'];
 const maxSize = 2000;
 
 export const uploadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
   if (event.target.files) {
     const file = event.target.files && event.target.files[0];
-    if (!allowedMimes.includes(file.type)) {
+    if (!types.includes(file.type)) {
       return 'Error - Only png,jpg,jpeg allowed';
     }
     if (file.size / 1024 > maxSize) {
